56a142b5dbaaab40c5c1c5583b5a828396ab3f65
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2020-06-05 11:01:28 -0600

    add genreator and prompts from veutify preset

---
+++ prompts.js
@@ -0,0 +1,15 @@
+module.exports = [
+  {
+    type: 'list',
+    name: 'preset',
+    message:
+      'Select from one of the following presets to scaffold your Vue project:',
+    choices: [
+      {
+        name: 'Base (A basic Vuetify application)',
+        value: 'base',
+      },
+    ],
+    default: 'base',
+  },
+]
